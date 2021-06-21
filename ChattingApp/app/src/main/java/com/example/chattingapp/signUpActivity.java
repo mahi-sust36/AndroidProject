3e341aac4ca4cc92782a012aff21c87d318f822e
Farhan Mahtab Mahi <farhan36@student.sust.edu>
2021-06-21 21:33:40 +0600

    added xml file for signUp and SignIn
    Created and added xml  for sign in activity and sign up activity page.Also added view binder.

--- ChattingApp/app/src/main/java/com/example/chattingapp/signUpActivity.java
+++ ChattingApp/app/src/main/java/com/example/chattingapp/signUpActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.chattingapp.databinding.ActivitySignUpBinding;
+
 public class signUpActivity extends AppCompatActivity {
+    ActivitySignUpBinding binding ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater()) ;
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
     }
 }
