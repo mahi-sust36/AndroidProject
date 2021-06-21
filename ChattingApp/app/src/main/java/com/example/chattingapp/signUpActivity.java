650920dc2489f6a1afd660da6c84dc6fb17c43f0
Farhan Mahtab Mahi <farhan36@student.sust.edu>
2021-06-22 01:37:00 +0600

    Added firebase database
    Added Firebase data ,
    error :"-socketexception-socket-failed-eperm-operation-not-permitted" needs to be fixed.

--- ChattingApp/app/src/main/java/com/example/chattingapp/signUpActivity.java
+++ ChattingApp/app/src/main/java/com/example/chattingapp/signUpActivity.java
@@ -1,14 +1,24 @@
 package com.example.chattingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.chattingapp.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.Context;
 
 public class signUpActivity extends AppCompatActivity {
     ActivitySignUpBinding binding ;
-
+    private FirebaseAuth auth;
+    FirebaseDatabase databse ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +26,26 @@ public class signUpActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
+        auth = FirebaseAuth.getInstance();
+        databse = FirebaseDatabase.getInstance() ;
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.createUserWithEmailAndPassword
+                        (binding.etEmail.getText().toString() ,binding.etPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+
+                                Toast.makeText(signUpActivity.this,"Account created successfully.",Toast.LENGTH_SHORT).show();
+                            }else
+                            {
+                                Toast.makeText(signUpActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                    }
+                });
+            }
+        });
     }
 }
