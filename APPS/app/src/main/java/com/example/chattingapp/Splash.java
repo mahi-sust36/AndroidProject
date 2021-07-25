0732b80a6102b81cfe15e892d126a779f2d459ee
Fahim Iftekhar Efto <ifto.141.fi@gmail.com>
2021-07-26 01:08:52 +0600

    Splash Screen Added

---
+++ APPS/app/src/main/java/com/example/chattingapp/Splash.java
@@ -0,0 +1,30 @@
+package com.example.chattingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+    private static int SPLASH_SCREEN =4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,signInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
--- APPS/app/src/main/java/com/example/chattingapp/signInActivity.java
+++ APPS/app/src/main/java/com/example/chattingapp/signInActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.chattingapp.Models.Users;
@@ -33,6 +34,7 @@ public class signInActivity extends AppCompatActivity {
     FirebaseDatabase database ;
 
     GoogleSignInClient mGoogleSignInClient ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
